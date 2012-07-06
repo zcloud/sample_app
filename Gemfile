0b46dbc430724439dbbff9b54e111bbcaf27a814
SSS <cloud@zurya.com>
2012-07-05 20:48:43 -0700

    finish Ch7.1

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'bcrypt-ruby',  '3.0.1'
 
 group :test do
 	gem 'capybara', '1.1.2'
+	gem 'factory_girl_rails', '3.5.0'
 end
 
 group :development do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 			"Aina Sa"
+		email 				"aina@test.com"
+		password	"billdave"
+		password_confirmation	"billdave"
+	end
+end
