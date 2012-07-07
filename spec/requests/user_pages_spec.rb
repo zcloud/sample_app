c4bc24831eb325116785ead5c4611712aa96afc2
SSS <SSS@server>
2012-07-07 16:13:06 -0700

    Finish Signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,23 +16,102 @@ describe "User pages" do
 		before { visit signup_path } 
 		
 		let(:submit) { "Create my account" } 
+		
 		describe "with invalid information" do 
+			
 			it "should not create a user" do 
 				expect { click_button submit }.not_to change(User, :count) 
 			end 
+
+			describe "error messages" do
+				describe "empty name" do
+					before do
+						fill_in "Name", with: ""
+						fill_in "Email", with: "user1@example.com"
+						fill_in "Password", with: "foobar"
+						fill_in "Confirmation", with: "foobar"
+						click_button submit 
+					end
+					it { should have_selector('title', text: 'Sign up') }
+					it { should have_content('error') }
+					it { should have_content("Name can't be blank") }
+				end
+				
+				describe "empty email" do
+					before do
+						fill_in "Name", with: "test user"
+						fill_in "Email", with: ""
+						fill_in "Password", with: "foobar"
+						fill_in "Confirmation", with: "foobar"
+						click_button submit 
+					end
+					it { should have_selector('title', text: 'Sign up') }
+					it { should have_content('error') }
+					it { should have_content("Email can't be blank") }
+				end
+				
+				describe "empty password" do
+					before do
+						fill_in "Name", with: "test user"
+						fill_in "Email", with: "test@test.com"
+						fill_in "Password", with: ""
+						fill_in "Confirmation", with: "foobar"
+						click_button submit 
+					end
+					it { should have_selector('title', text: 'Sign up') }
+					it { should have_content('error') }
+					it { should have_content("Password can't be blank") }
+
+				end
+				
+				describe "empty password confirmation" do
+					before do
+						fill_in "Name", with: "test user"
+						fill_in "Email", with: "user1@example.com"
+						fill_in "Password", with: "foobar"
+						fill_in "Confirmation", with: ""
+						click_button submit 
+					end
+					it { should have_selector('title', text: 'Sign up') }
+					it { should have_content('error') }
+					it { should have_content("Password confirmation can't be blank") }
+				end
+				
+				describe "short password" do
+					before do
+						fill_in "Name", with: "test user"
+						fill_in "Email", with: "user1@example.com"
+						fill_in "Password", with: "foo"
+						fill_in "Confirmation", with: "foo"
+						click_button submit 
+					end
+					it { should have_selector('title', text: 'Sign up') }
+					it { should have_content('error') }
+					it { should have_content("Password is too short (minimum is 6 characters)") }
+				end
+			end
 		end 
 		
 		describe "with valid information" do 
-			before do
-				fill_in "Name", with: "test user"
-				fill_in "Email", with: "user1@example.com"
-				fill_in "Password", with: "foobar"
-				fill_in "Confirmation", with: "foobar"
+			describe "unique email" do
+				before do
+					fill_in "Name", with: "test user"
+					fill_in "Email", with: "user@example.com"
+					fill_in "Password", with: "foobar"
+					fill_in "Confirmation", with: "foobar"
+				end
+			
+				it "should create a user" do 
+					expect { click_button submit }.to change(User, :count).by(1) 
+				end
+				
+				describe "after saving the user" do
+					before { click_button submit }
+					let(:user) { User.find_by_email('user@example.com') }
+					it { should have_selector('title', text: user.name) }
+					it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+				end
 			end
-		
-			it "should create a user" do 
-				expect { click_button submit }.to change(User, :count).by(1) 
-			end
-		end		
+		end
 	end
 end
