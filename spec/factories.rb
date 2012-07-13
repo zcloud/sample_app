9572c0eb9d6a046714a3cacbde07f1d873ae96eb
SSS <SSS@server>
2012-07-13 01:43:22 -0700

    finish ch 9.4

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,12 @@
 FactoryGirl.define do
-factory :user do
-    sequence(:name)  { |n| "User#{n}" }
-    sequence(:email) { |n| "user#{n}@test.com"}
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
-  end
 
+    factory :admin do
+      admin true
+    end
+  end
 end
