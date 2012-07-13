68d5b6cef2ad1519b252605811e2aa0675545bc9
SSS <SSS@server>
2012-07-13 05:40:22 -0700

    finish ch 10.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,11 @@
 #
 
 class User < ActiveRecord::Base 
-	attr_accessible :name, :email, :password, :password_confirmation 
-	has_secure_password 
+	attr_accessible :name, :email, :password, :password_confirmation
+  has_secure_password
+  has_many :microposts, dependent: :destroy
+
+	
 	before_save { |user| user.email = email.downcase } 
 	before_save :create_remember_token
 	validates :name, presence: true, length: { maximum: 50 } 
@@ -19,6 +22,11 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
 	validates :password, length: { minimum: 6 } 
 	validates :password_confirmation, presence: true 
+
+	def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
 	private
 
