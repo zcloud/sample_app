3c877570f7bf8a144e674245f1a36bb16d022b99
SSS <SSS@server>
2012-07-13 00:15:40 -0700

    finish 9.3.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,22 +29,22 @@ describe "Authentication" do
 		
 		describe "with valid info" do
 			let(:user) { FactoryGirl.create(:user) }
-			
-			before { sign_in(user) }
-			
-			it { should have_selector('title', text: user.name) }
-			it { should have_link('Profile', href: user_path(user) ) }
-			it { should have_link('Settings', href: edit_user_path(user) ) }
-			it { should have_link('Sign out', href: signout_path) }
-			it { should_not have_link('Sign in', href: signin_path) }
+      before { sign_in user }
+
+      it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+
+      it { should_not have_link('Sign in', href: signin_path) }
 
 			describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
       end
 		end
-		
-	
 	end
 	
 
@@ -63,7 +63,11 @@ describe "Authentication" do
 					before { put user_path(user) }
 					specify { response.should redirect_to(signin_path) }
 				end
-				
+
+				describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
 			end
 			
       describe "when attempting to visit a protected page" do
