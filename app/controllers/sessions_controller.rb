a1e9dcd96c0ec4ccd01b4a5f8404a624c19474da
SSS <SSS@server>
2012-07-12 23:30:34 -0700

    fixed errors in ch9.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	def new
 	end
 	
-def create
+	def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_filter :signed_in_user, only: [:edit, :update]
-	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :correct_user,   	only: [:edit, :update]
 	
 	def show
 		@user = User.find(params[:id])
