15fbdc6fc1d08745af4271a82e61d0b55bd7f08f
SSS <SSS@server>
2012-07-22 19:17:06 -0700

    finish ch11.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,7 +49,6 @@ describe "Authentication" do
 	
 
 	describe "authorization" do
-
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -68,9 +67,31 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+				
+				describe "visiting the following page" do
+					before { visit following_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
+				end
+
+				describe "visiting the followers page" do
+					before { visit followers_user_path(user) }
+					it { should have_selector('title', text: 'Sign in') }
+				end
 			end
 			
-      describe "when attempting to visit a protected page" do
+			describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+			
+			describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
           fill_in "Email",    with: user.email
